4d7782db4d690d4bced4a5feb5a5441d6a4cabc1
Corentin Marzin <cmarzin@ubaq.io>
2025-08-24 18:48:09 +0200

    update my photo finally, update link project

--- components/Project/Box/index.vue
+++ components/Project/Box/index.vue
@@ -1,46 +1,53 @@
 <template>
   <div class="project-box__container">
-
     <page-dots
       v-if="!mediaSize('MobileLg')"
       @updateCurrentProjecToLeft="$emit('updateCurrentProjectToLeft')"
-      @updateCurrentProjecToRight="$emit('updateCurrentProjectToRight')" />
+      @updateCurrentProjecToRight="$emit('updateCurrentProjectToRight')"
+    />
 
     <h1 class="project-box__title">{{ projectData.title }}</h1>
     <h2 class="project-box__subtitle">{{ projectData.projectType }}</h2>
 
-    <custom-link custom-class="project-box__project-button" :link="projectData.externalUrl" external />
+    <custom-link
+      custom-class="project-box__project-button"
+      :link="projectData.externalUrl"
+      external
+    />
 
-    <div class="project-box__description" v-html="projectData.shortDescription"></div>
-
+    <div
+      class="project-box__description"
+      v-html="projectData.shortDescription"
+    ></div>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex'
 
-  export default {
-    props: {
-      projectData: {
-        type: Object,
-        default: () => ({
-          title: 'YSF',
-          projectType: 'Agency website',
-          date: '',
-          externalUrl: 'https://ysf.cocoweb.fr/',
-          localUrl: '/ysf',
-          background: 'ysf.png',
-          shortDescription: 'Création d\'un site d\'agence web pour des copains de promotions.',
-          stack: '',
-        }),
-      },
+export default {
+  props: {
+    projectData: {
+      type: Object,
+      default: () => ({
+        title: 'YSF',
+        projectType: 'Agency website',
+        date: '',
+        externalUrl: 'https://ysf.cocommit.fr/',
+        localUrl: '/ysf',
+        background: 'ysf.png',
+        shortDescription:
+          "Création d'un site d'agence web pour des copains de promotions.",
+        stack: '',
+      }),
     },
-    computed: {
-      ...mapGetters({
-        mediaSize: 'ui/getCurrentMediaSize'
-      })
-    },
-  }
+  },
+  computed: {
+    ...mapGetters({
+      mediaSize: 'ui/getCurrentMediaSize',
+    }),
+  },
+}
 </script>
 
 <style scoped src="./_style.scss" lang="scss"></style>
